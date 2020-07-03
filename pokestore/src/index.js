f344d07425db2a420bee62cd4e44c56592b31c0e
Camila Chung <MilaChung@users.noreply.github.com>
2020-07-03 14:49:51 -0300

    [POKESTORE] Adding the pokemon function

--- pokestore/src/index.js
+++ pokestore/src/index.js
@@ -1,6 +1,19 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+//Function to call the pokemon and price
+const Pokemon = props => {
+  //Initialing the value of variable pokemon and cart button
+  const { pokemon, addCart} = props;
+
+  return (
+    <div>
+      {pokemon.name} ${pokemon.price}
+      {addCart}
+    </div>
+  );
+};
+
 ReactDOM.render(
     <h1>Pokestore</h1>,
   document.getElementById('root')
