bd63cab1625eb883a71e6c2a4858e0ae57693c2e
Camila Chung <MilaChung@users.noreply.github.com>
2020-07-03 15:07:38 -0300

    [APP] Initialing app with states of pokemon and cart

--- pokestore/src/index.js
+++ pokestore/src/index.js
@@ -14,6 +14,19 @@ const Pokemon = props => {
   );
 };
 
+function App() {
+  //Initialing the state of obj pokemon 
+  const [pokemon] = useState ([
+    { name: "Pikachu", price: 100 },
+    { name: "Charmander", price: 200 },
+    { name: "Squirtle", price: 300 },
+    { name: "Bulbasaur", price: 500 }
+  ]);
+
+  //Initialing the state of cart 
+  const [cart, setCart] = useState([]);
+}
+
 ReactDOM.render(
     <h1>Pokestore</h1>,
   document.getElementById('root')
