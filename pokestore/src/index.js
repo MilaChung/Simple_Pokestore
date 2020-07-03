4262632a5f3c51ebb511155435b37c9db0c09502
Camila Chung <MilaChung@users.noreply.github.com>
2020-07-03 19:12:50 -0300

    [POKESTORE] Finished simple Pokemon store app (with style errors)

--- pokestore/src/index.js
+++ pokestore/src/index.js
@@ -5,24 +5,29 @@ import "./style.css"
 //Function to call the pokemon and price
 const Pokemon = props => {
   //Initialing the value of variable pokemon and cart button
-  const { poke, children } = props;
+  const { photo, poke, children } = props;
 
   return (
-    <div className="pokemon">
-      {poke.name} ${poke.price}
-      {children}
+    <div className="poke">
+      <img className="photo" src={poke.photo}/>
+        {poke.name} ${poke.price}
+        {children}
     </div>
   );
 };
 
+//Function to call the cart 
 const Cart = props => {
-  const { poke, children } = props;
+  const { photo, poke, children } = props;
 
   return (
-    <ul>
+    <ul className="item">
       <li>
-        {poke.name} ${poke.price}
-        {children}
+        <img className="photo-cart" src={poke.photo}/>
+        <div>
+          {poke.name} ${poke.price}
+          {children}
+        </div>
       </li>
     </ul>
   );
@@ -31,10 +36,9 @@ const Cart = props => {
 function App() {
   //Initialing the state of obj pokemon 
   const [pokemon] = useState ([
-    { name: "Pikachu", price: 100 },
-    { name: "Charmander", price: 200 },
-    { name: "Squirtle", price: 300 },
-    { name: "Bulbasaur", price: 500 }
+    { photo: "https://pokeres.bastionbot.org/images/pokemon/4.png", name: "Charmander", price: 200 },
+    { photo: "https://pokeres.bastionbot.org/images/pokemon/7.png", name: "Squirtle", price: 300 },
+    { photo: "https://pokeres.bastionbot.org/images/pokemon/1.png", name: "Bulbasaur", price: 500 }
   ]);
 
   //Initialing the state of cart 
@@ -56,24 +60,24 @@ function App() {
       <hr />
 
       <div className="app">
-        <div className="poke">
+        <div className="pokemon">
           {/* Show the pokemon list */}
           {pokemon.map((poke, pokeAdded) => (
             <Pokemon key={pokeAdded} poke={poke}>
               {/* Button with event, when are clicked call the function addToCart */}
-              <button onClick={() => addToCart(pokeAdded)}>Add to cart</button>
+              <button className="button" onClick={() => addToCart(pokeAdded)}>Add to cart</button>
             </Pokemon>
           ))}
         </div>
 
         {/* Show the total price of cart */}
         <div  className="cart">
-          Cart Total: ${calculatePrice()}
           {cart.map((poke, pokeAdded) => (
             <Cart key={pokeAdded} poke={poke}>
               {" "}
             </Cart>
           ))}
+          <div className="total">Cart Total: ${calculatePrice()}</div>
         </div>
       </div>
     </div>
